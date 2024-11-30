b9d6d671cbfeeb6982a8668564f7cdfe0143f79b
Lari7l <larissa.rfrasseto@gmail.com>
2024-11-29 22:36:38 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -35,5 +35,12 @@ public class Rental {
                  break;
           }
           return thisAmount;
-     }
+   }
+   public int getFrequentRenterPoints() { 
+      // add frequent renter points
+      if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+         return 2;
+      }
+      return 1;
+   }
 }
