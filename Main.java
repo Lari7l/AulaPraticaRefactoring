46e33ff16623a2a2b44e20c408a0d9368ff8fd5c
Lari7l <larissa.rfrasseto@gmail.com>
2024-11-29 22:16:00 -0300

    Commit 3

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+* Engenharia de Software Moderna 
+* Prof. Marco Tulio Valente
+* 
+* Roteiro Prático sobre refactoring
+* 
+* Classes iniciais do sistema de videolocadora
+*
+*/
+class Main {
+    public static void main(String[] args) {
+      System.out.println("Apenas um exemplo de refatoração...");
+    }
+  }
